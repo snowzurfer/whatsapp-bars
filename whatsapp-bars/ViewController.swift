79b3743fed5acb8cf07caf20d7feae840f5ad3c1
Alberto Taiuti <alberto.taiuti@gmail.com>
2018-10-28 09:47:09 +0000

    Initial Commit

---
+++ whatsapp-bars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatsapp-bars
+//
+//  Created by Alberto Taiuti on 28/10/2018.
+//  Copyright © 2018 Alberto Taiuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
